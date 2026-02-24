a2be8e3a2d0e8e59e2a062f26f41837dffce8241
Tuffy90 <misha.gvozdenko11@gmail.com>
2026-02-24 16:11:59 +0100

    Update Snake Game: Final version with vx/vy and buffering

--- game.c
+++ game.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <SDL2/SDL.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
 #define VERSION "0.0.1 Alpha"
 #define WIDTH 10
 #define HEIGHT 10
@@ -32,46 +34,40 @@ typedef struct{
     int Wh;
 }Wall;
 
-int main(int argc, char* argv[]){
-    if(SDL_Init(SDL_INIT_VIDEO)< 0){
-        printf("SDL ERROR", SDL_GetError());
-        return 1;
-    }
+int main(){
     Map map = {20,20};
     Wall wall = {0,0,20,20};
     Food food = {rand() % map.w, rand() % map.h};
-    Snake head = {5,5};
+    Snake head = {10, 10};
+    int vx = 1, vy = 0; 
     bool running = true;
-    char buffer [3000];
+    char buffer[3000];
     int n = 0;
-    printf("\033[H");
+    
+    srand(time(NULL)); 
 
-    SDL_Event event;
-   while(running){
-    n = 0;
-    system("cls");
-    while(SDL_PollEvent(&event)){
-     if (event.type == SDL_QUIT) running = false;
-        if (event.type == SDL_KEYDOWN) {
-            switch(event.key.keysym.sym) {
-                case SDLK_ESCAPE: 
-                running = false; 
-                break;
-                case SDLK_UP:
-                head.y--;
-                break;
-                case SDLK_DOWN:
-                head.y++;
-                break;
-                case SDLK_LEFT:
-                head.x--;
-                break;
-                case SDLK_RIGHT:
-                head.x++;
-                break;
+    while (running) {
+        if (_kbhit()) {
+            char key = _getch();
+            switch (key) {
+                case 'w': if (vy == 0) { vx = 0; vy = -1; } break;
+                case 's': if (vy == 0) { vx = 0; vy = 1; } break;
+                case 'a': if (vx == 0) { vx = -1; vy = 0; } break;
+                case 'd': if (vx == 0) { vx = 1; vy = 0; } break;
+                case 'q': running = false; break;
             }
         }
-     }
+
+        head.x += vx;
+        head.y += vy;
+
+
+        if (head.x <= 0 || head.x >= map.w - 1 || head.y <= 0 || head.y >= map.h - 1) {
+            running = false; 
+        }
+
+        n = 0;
+        printf("\033[H"); 
      for(int i = 0; i < map.w; i++){
         for(int j = 0; j < map.h; j++){
             if(i == head.y && j == head.x) buffer[n++] = 'H';
@@ -83,7 +79,7 @@ int main(int argc, char* argv[]){
     }
     buffer[n] = '\0';
     printf("%s", buffer); 
-     SDL_Delay(500);
+    Sleep(150); 
     }
 
     getchar();
